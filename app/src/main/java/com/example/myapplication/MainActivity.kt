4a8e49ed759f95e6ba2824de1cc3ba4af9f59679
bandit <bandit@mail.com>
2026-06-30 18:05:44 +0200

    MapLibre engine implementiert, Quelle für Kartendaten (diese URL in der MainActivity) klappt auch

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -59,7 +60,7 @@ fun MapScreen(modifier: Modifier = Modifier) {
     )
 
     MaplibreMap(
-        modifier = modifier.fillMaxSize().testTag("maplibre_map"),
+        modifier = Modifier.fillMaxSize().testTag("maplibre_map"),
         baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
         cameraState = cameraState
     )
